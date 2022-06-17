892b92837ddf0a08d4b813d9cb58fbd2d9769463
Renars Miljons <reku.miljons@gmail.com>
2022-06-17 22:17:55 +0300

    bot replies and reacts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -10,10 +10,17 @@ client.once('ready', () => {
 });
 
 client.on("messageCreate", (message) => {
+    console.log(message.author.id);
     if(message.content.startsWith("!start")) {
-        message.channel.send("Hello!");
+        message.channel.send("the embed thing goes here");  
     }
-})
+    if (message.author.id === '987373655715639316' && message.content === 'the embed thing goes here') {
+        message.react('👏');
+    }
+}) 
+
+
+
 
 
 
