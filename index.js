61e331f4fcd0cdf7e0a7b285eecc199398240332
CitaZane <zanekrumina2@gmail.com>
2022-06-18 00:31:43 +0300

    conflict

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
 
-// temp holds new channel ID
+// temp variable holds new channel ID
  var channelID
  
 // When the client is ready, run this code (only once)
@@ -12,30 +12,25 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-let lobbyOpen = false;
-
-client.on("messageCreate",  (message) => {
-    console.log(message.author.id);
-    if(message.content.startsWith("!start") && lobbyOpen === false) {
-        lobbyOpen = true;
-        message.channel.send("the embed thing goes here");  
+client.on("messageCreate", async (message) => {
+    if(message.content.startsWith("!start")) {
+        message.channel.send("Hello!");
     }
-    if (message.author.id === '987373655715639316' && message.content === 'the embed thing goes here') {
-        message.react('👏');
-        setTimeout(() => {
-            if(lobbyOpen === true) {
-            message.channel.send('Lobby closed, type !start to open a new Lobby');
-            }
-        }, 30000);
+    // temp command for creating new channel
+    if(message.content.startsWith("!channel")) {
+        channelID = await createNewChannel(message)
+    }
+    // temp command for deleteing channel
+    if(message.content.startsWith("!delete")){
+        deleteChannel(channelID)
     }
 }) 
 
-// 
-// create new channel and return id
+// create new channel and return its id
 const createNewChannel = async (message)=>{
     let result = await message.guild.channels.create('Dark Corner',{
         type: "GUILD_TEXT",
-         permissionOverwrites: [{ // same as before
+         permissionOverwrites: [{
             id: message.guild.id,
             allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
         }]
