5f81008ece63461ebc37e4f8a83a6e5f315cf28d
CitaZane <zanekrumina2@gmail.com>
2022-06-18 00:09:00 +0300

    merge

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
 
-// temp variable holds new channel ID
+// temp holds new channel ID
  var channelID
  
 // When the client is ready, run this code (only once)
@@ -12,25 +12,36 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+let lobbyOpen = false;
+
 client.on("messageCreate", async (message) => {
-    if(message.content.startsWith("!start")) {
-        message.channel.send("Hello!");
+    console.log(message.author.id);
+    if(message.content.startsWith("!start") && lobbyOpen === false) {
+        lobbyOpen = true;
+        message.channel.send("the embed thing goes here");  
     }
-    // temp command for creating new channel
-    if(message.content.startsWith("!channel")) {
+    if (message.author.id === '987373655715639316' && message.content === 'the embed thing goes here') {
+        message.react('👏');
+        setTimeout(() => {
+            if(lobbyOpen === true) {
+            message.channel.send('Lobby closed, type !start to open a new Lobby');
+            }
+        }, 30000);
+    }
+        if(message.content.startsWith("!channel")) {
         channelID = await createNewChannel(message)
     }
-    // temp command for deleteing channel
     if(message.content.startsWith("!delete")){
         deleteChannel(channelID)
     }
-})
+}) 
 
-// create new channel and return its id
+
+// create new channel and return id
 const createNewChannel = async (message)=>{
     let result = await message.guild.channels.create('Dark Corner',{
         type: "GUILD_TEXT",
-         permissionOverwrites: [{
+         permissionOverwrites: [{ // same as before
             id: message.guild.id,
             allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
         }]
@@ -45,6 +56,9 @@ const deleteChannel =  (id)=>{
       channel.delete()
 }
 
+//1st commit from Siva
+//raimonds
+// main
 
 // Login to Discord with your client's token
 client.login(token);
