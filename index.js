5b7814b73ecb723d0e2216620482069d67ff57f2
CitaZane <zanekrumina2@gmail.com>
2022-06-17 23:45:10 +0300

    Create / delete new channel

--- index.js
+++ index.js
@@ -4,17 +4,44 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
 
+// temp holds new channel ID
+ var channelID
+ 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
     if(message.content.startsWith("!start")) {
         message.channel.send("Hello!");
     }
+    if(message.content.startsWith("!channel")) {
+        channelID = await createNewChannel(message)
+    }
+    if(message.content.startsWith("!delete")){
+        deleteChannel(channelID)
+    }
 })
 
+// create new channel and return id
+const createNewChannel = async (message)=>{
+    let result = await message.guild.channels.create('Dark Corner',{
+        type: "GUILD_TEXT",
+         permissionOverwrites: [{ // same as before
+            id: message.guild.id,
+            allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+        }]
+    })
+    return result.id
+}
+//  delete channel by id 
+const deleteChannel =  (id)=>{
+    const channel = client.channels.cache.filter((channel) => {
+        return channel.id === id
+      }).first()
+      channel.delete()
+}
 
 
 // Login to Discord with your client's token
