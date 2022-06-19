a064a64ec6fdeb39597782b91ff1df1e6db6af5d
Renars Miljons <reku.miljons@gmail.com>
2022-06-19 10:52:54 +0300

    final

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Client({
   ],
 });
 
+/*
 const exampleEmbed = new MessageEmbed()
   .setColor("#FFFF00")
   .setTitle("Mafia")
@@ -19,6 +20,8 @@ const exampleEmbed = new MessageEmbed()
     "https://media.istockphoto.com/photos/noir-movie-character-picture-id837345268?k=20&m=837345268&s=612x612&w=0&h=1tahuBSTIUCUbVcZhaxHMV5iLm-W1c_UBlz7VBAcNrc="
   )
   .setTimestamp();
+
+  */
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -29,6 +32,7 @@ const game = {};
 
 game.lobbyOpen = false;
 game.participants = [];
+game.votingActive = false;
 game.participantRoles = {
   mafia: "",
   doctor: "",
@@ -41,7 +45,6 @@ game.roomID = ""; //game room
 game.nominationActive = false;
 let nominations = {};
 
-// botID = '987427488009433108' // Zane Bot
 botID = "987373655715639316"; //Mafia Bot
 
 client.on("messageCreate", (message) => {
@@ -52,7 +55,9 @@ client.on("messageCreate", (message) => {
       /*{ embeds: [exampleEmbed]}*/ "Game Lobby open, react to this message to participate!"
     );
   }
-
+  if (message.content.startsWith("!nominate") && game.nominationActive) {
+     countVote(message);   
+  }
   /* ------------------------------- open lobby ------------------------------- */
   if (
     message.author.id === botID &&
@@ -60,17 +65,22 @@ client.on("messageCreate", (message) => {
   ) {
     message.react("👏");
 
-    const waitForFullLobby = setInterval(() => {
+    const waitForFullLobby = setInterval(async () => {
       if (game.participants.length === 5) {
         message.channel.sendTyping();
-        // game.participants = [];
         clearInterval(waitForFullLobby);
-        lobbyOpen = false;
+        game.lobbyOpen = false;
         assignRoles();
         sendRoles(message);
         message.channel.send(
-          "Lobby Closed! Game number (id) started successfully!\n A new game channel (id) has been created with all the participants"
+          "Lobby Closed! Mafia has contacted you and the killer is on the run..."
         );
+
+        // sends a message to the newly created channel
+        game.roomID = await createNewChannel(message);
+        const channel = getChannel()
+        channel.send("Hello players!");
+        startNight(message);
       }
     }, 1);
     setTimeout(() => {
@@ -86,6 +96,9 @@ client.on("messageCreate", (message) => {
   }
 });
 
+const votingResults = {}
+
+
 client.on("messageReactionAdd", (messageReaction, user) => {
   if (messageReaction.emoji.name == "👏" && user != "987373655715639316") {
     game.participants.push(user);
@@ -94,7 +107,17 @@ client.on("messageReactionAdd", (messageReaction, user) => {
 
 // When night starts
 
-const startNight = (message) => {
+const startNight = () => {
+  // inform players
+  const channel = getChannel()
+  channel.send("The night has started, civilians can safely go to sleep...");
+  // track messages
+  let currentMessage = '';
+  client.on("messageCreate", (message) => {
+    currentMessage = message
+  })
+  
+  console.log("This: ", currentMessage);
   let mafiaVoted = false;
   let doctorVoted = false;
   let detectiveVoted = false;
@@ -105,47 +128,62 @@ const startNight = (message) => {
   let detectiveRevealed;
 
   // each player gets sent a list of people to kill/save/reveal
+  //console.log("Game participants: ",game.participants);
   let mafiaList = game.participants.filter(player => player != game.participantRoles.mafia);
   let doctorList = game.participants.filter(player => player != game.participantRoles.doctor);
   let detectiveList = game.participants.filter(player => player != game.participantRoles.detective);
+  setTimeout(() => {
   game.participantRoles.mafia.send(`Who do you want to kill? 🔪 \n 1. ${mafiaList[0]} \n 2. ${mafiaList[1]} \n 3. ${mafiaList[2]} \n 4. ${mafiaList[3]} \n Reply with a number (1-4):`);
   game.participantRoles.doctor.send(`Who do you want to save? ❤️ \n 1. ${doctorList[0]} \n 2. ${doctorList[1]} \n 3. ${doctorList[2]} \n 4. ${doctorList[3]} \n Reply with a number (1-4):`);
   game.participantRoles.detective.send(`Who do you want to reveal? 👁️ \n 1. ${detectiveList[0]} \n 2. ${detectiveList[1]} \n 3. ${detectiveList[2]} \n 4. ${detectiveList[3]} \n Reply with a number (1-4):`);
-  
+  }, 100);
 
   //this interval checks who voted for whom
   const listenForAllVotes = setInterval(()=>{
-    if(message.content == "1" || message.content == "2" || message.content == "3" || message.content == "4") {
-      if(message.author == game.participantRoles.mafia) {
-        mafiaKilled = mafiaList[Number(message.content)];
+    //console.log("Interval started");
+    if(currentMessage.content == "1" || currentMessage.content == "2" || currentMessage.content == "3" || currentMessage.content == "4") {
+      if(currentMessage.author == game.participantRoles.mafia) {
+        mafiaKilled = mafiaList[Number(currentMessage.content)-1];
         mafiaVoted = true;
+        //console.log("Mafia Voted: " + mafiaVoted);
       }
-      if(message.author == game.participantRoles.doctor) {
-        doctorSaved = doctorList[Number(message.content)];
+      if(currentMessage.author == game.participantRoles.doctor) {
+        doctorSaved = doctorList[Number(currentMessage.content)-1];
         doctorVoted = true;
+        //console.log("Mafia Voted: " + mafiaVoted);
       }
-      if(message.author == game.participantRoles.detective) {
-        detectiveRevealed = detectiveList[Number(message.content)];
+      if(currentMessage.author == game.participantRoles.detective) {
+        detectiveRevealed = detectiveList[Number(currentMessage.content)-1];
         detectiveVoted = true;
+        //console.log("Mafia Voted: " + mafiaVoted);
       }
     }
     if(mafiaVoted && doctorVoted && detectiveVoted) {
       clearInterval(listenForAllVotes);
-
       //send the detective role of the revealed player
       for (const [key, value] of Object.entries(game.participantRoles)) {
-        if (value == detectiveRevealed) {
-          game.participantRoles.detective.send(`${detectiveRevealed} is: ${key}`);
+        if (Array.isArray(value)) {
+          game.participantRoles.detective.send(`${detectiveRevealed} is: Civilian`);
+        } else {
+          if (value == detectiveRevealed) {
+            game.participantRoles.detective.send(`${detectiveRevealed} is: ${key}`);
+          }
         }
       }
       
       // calculate who is killed
       if (mafiaKilled == doctorSaved) {
+          getChannel().send("Everyone survived the Night! 🎉 \n Type 🔪**!nominate**🔪 @player_name if you're suspicious. 🤔");
         // bot says everyone survived!
       } else {
+        getChannel().send(`${mafiaKilled} got killed tonight! ☠️ \n Who could it be?... 🤔 \n Type 🔪**!nominate**🔪 @player_name if you're suspicious.`);
         // player that is eliminated: mafiaKilled;
       }
+
       // start the day
+      setTimeout(() => {
+        activateNominations();
+      }, 1000);
     }
   },1);
 }
@@ -162,18 +200,15 @@ const activateNominations = (message) => {
   game.topNominations = [];
   nominations = {};
   // open voting
-  message.channel.sendTyping();
-  message.channel.send("Voting has started. You have 30 seconds to vote!");
-  message.channel.send("Vote -> !n @mention");
   game.nominationActive = true;
   // create timeout to stop voting
   setTimeout(() => {
     if (game.nominationActive) {
       game.nominationActive = false;
-      message.channel.send("Votes recived. Stop Voting now.");
+      getChannel().send("Voting ended!");
       collectVotes();
     }
-  }, 20000);
+  }, 200000);
 };
 
 // register vote with nomination
@@ -193,35 +228,40 @@ const collectVotes = () => {
     }
   });
   // votes counted, select top 3
+  const channel = getChannel()
   const playerArray = Object.entries(topFilter);
   playerArray.sort(([key, value1], [key2, value2]) => value2 - value1);
-  playerArray.forEach((player, index) => {
-    if (index >= 3) return;
-    game.topNominations.push(player[0]);
-  });
-  console.log("Top Three Selected: ", game.topNominations);
-  /* ------------- top three selected, create final woting message ------------ */
+  for(let i=0; i < game.participants.length; i++) {
+    if (game.participants[i].id == playerArray[0][0]) {
+      channel.send(`${game.participants[i]} got eliminated! ☠️`);
+      game.participants = game.participants.filter(player => player != game.participants[i]);
+      startNight();
+      break;
+    }
+  }
+
 };
 
 //assign roles to each participant
 const assignRoles = () => {
+  let clone = [...game.participants];
   const mafia =
-    game.participants[Math.floor(Math.random() * game.participants.length)];
+    clone[Math.floor(Math.random() * clone.length)];
   game.participantRoles.mafia = mafia;
-  game.participants = game.participants.filter((player) => player != mafia);
+  clone = clone.filter((player) => player != mafia);
   const doctor =
-    game.participants[Math.floor(Math.random() * game.participants.length)];
+    clone[Math.floor(Math.random() * clone.length)];
   game.participantRoles.doctor = doctor;
-  game.participants = game.participants.filter((player) => player != doctor);
+  clone = clone.filter((player) => player != doctor);
   const detective =
-    game.participants[Math.floor(Math.random() * game.participants.length)];
+    clone[Math.floor(Math.random() * clone.length)];
   game.participantRoles.detective = detective;
-  game.participants = game.participants.filter((player) => player != detective);
+  clone = clone.filter((player) => player != detective);
   const civilian =
-    game.participants[Math.floor(Math.random() * game.participants.length)];
+    clone[Math.floor(Math.random() * clone.length)];
   game.participantRoles.civilians.push(civilian);
-  game.participants = game.participants.filter((player) => player != civilian);
-  game.participantRoles.civilians.push(game.participants[0]);
+  clone = game.participants.filter((player) => player != civilian);
+  game.participantRoles.civilians.push(clone[0]);
 };
 
 const sendRoles = (message) => {
@@ -251,7 +291,7 @@ const createNewChannel = async (message) => {
       },
     ],
   });
-  game.roomID = result.id;
+  return result.id;
 };
 //  delete channel by id
 const deleteChannel = (id) => {
@@ -262,6 +302,15 @@ const deleteChannel = (id) => {
     .first();
   channel.delete();
 };
+
+const getChannel= ()=> {
+  return  client.channels.cache
+    .filter((channel) => {
+      return channel.id === game.roomID;
+    })
+    .first();
+
+}
 
 // Login to Discord with your client's token
 client.login(token);
